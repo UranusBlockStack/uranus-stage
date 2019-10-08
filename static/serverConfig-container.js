b577b780e816c364d8e486cae4bab5af02ab0096
soaop <fengjin@uranus.io>
2019-10-08 10:04:06 +0800

    imageUrl in serverConfig-container.js

--- static/serverConfig-container.js
+++ static/serverConfig-container.js
@@ -1,5 +1,5 @@
 const serverConfig = {
   // apiUrl: 'https://54.180.158.219:4004/',
   apiUrl: '----->HOST_IP<-----',
-  imageServerUrl: 'http://127.0.0.1:3300/rancher-img/'
+  imageServerUrl: 'http://54.180.158.219:3300/rancher-img/'
 }
