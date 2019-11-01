e8f05df4c53af0a69d6d0521aacef93141d36b4f
Ann <liangyz@uranus.io>
2019-11-01 18:13:47 +0800

    fix js directory in index.html

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -129,7 +129,7 @@ module.exports = merge(common, {
     ],
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js',
+        filename: 'js/[name].[contenthash].js',
         path: path.resolve(__dirname, '../dist'),
         publicPath: '/',
     },
